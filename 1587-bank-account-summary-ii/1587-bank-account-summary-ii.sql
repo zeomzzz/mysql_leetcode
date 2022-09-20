ca7faecb6ed1d05e0445cb29ca59035b087f083e
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-09-20 23:30:56 +0900

    Time: 1393 ms (10.73%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -2,5 +2,5 @@ SELECT u.name
      , SUM(t.amount) AS balance
 FROM Users u
      INNER JOIN Transactions t ON u.account = t.account
-GROUP BY u.name
+GROUP BY u.account -- name으로 group  문제점 : name 중복. primary key는 account
 HAVING balance > 10000
