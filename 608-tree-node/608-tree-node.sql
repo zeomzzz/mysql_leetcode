38080b6f468dadb33c9d65352caa2f204488edf3
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-09-25 16:34:31 +0900

    Time: 539 ms (46.78%), Space: 0B (100.00%) - LeetHub

---
+++ 608-tree-node/608-tree-node.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH Child AS (
+SELECT p_id
+     , COUNT(*) child_cnt
+FROM Tree
+GROUP BY p_id
+)
+
+SELECT Tree.id
+     , CASE
+            WHEN Tree.p_id IS NULL THEN 'Root'
+            WHEN Tree.p_id IS NOT NULL AND Child.child_cnt IS NOT NULL THEN 'Inner'
+            ELSE 'Leaf'
+        END type
+FROM Tree
+     LEFT JOIN Child ON Tree.id = Child.p_id
+ORDER BY Tree.id
